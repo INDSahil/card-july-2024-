f22c63f23b9b394cc3218e45c48b95b7bfd1da35
INDSahil <sbains_be20@thapar.edu>
2024-07-26 16:20:25 +0530

    Initial commit

---
+++ src/Utils/Constants.jsx
@@ -0,0 +1,26 @@
+export const ProjectsCard = [
+  {
+    id: 1,
+    image: /* add image here */ "image",
+    title: "Project 1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit... ",
+  },
+  {
+    id: 2,
+    image: /*  */ "image",
+    title: "Project 2",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
+  },
+  {
+    id: 3,
+    image: /*  */ "image",
+    title: "Project 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit... ",
+  },
+  {
+    id: 4,
+    image: /*  */ "image",
+    title: "Project 4",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit... ",
+  },
+];
